600ddae60250e56a9f53c115e8d6d6a7592359c8
Milton Lima <milton.lima@outlook.com>
2020-08-19 13:14:25 -0300

    Desafio lista reversa e ordenada

---
+++ reverselist.py
@@ -0,0 +1,22 @@
+
+# Lista Inversa com a função reverse
+numbers = [1, 4, 8 ,0, 10, 20]
+numbers.reverse()
+print(numbers)
+
+# Lista Inversa Usando Slice
+numbers = [1, 4, 8 ,0, 10, 20]
+print(numbers[::-1])
+
+#Lista Inversa Ordenada com sort + reverse
+numbers = [1, 4, 8 ,0, 10, 20]
+numbers.sort(reverse=True)
+print(numbers)
+
+numbers = [1, 4, 8 ,0, 10, 20]
+print(list(reversed(numbers)))
+
+# -------- return ! 
+[20, 10, 0, 8, 4, 1]
+[20, 10, 0, 8, 4, 1]
+[20, 10, 8, 4, 1, 0]
