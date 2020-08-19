73f169edbe492bd4182c334b2467bd40d590c63c
Milton Lima <milton.lima@outlook.com>
2020-08-19 13:54:42 -0300

    Lista reversa com funções reverse, reversed, slice e sort

--- reverselist.py
+++ reverselist.py
@@ -13,6 +13,7 @@ numbers = [1, 4, 8 ,0, 10, 20]
 numbers.sort(reverse=True)
 print(numbers)
 
+# Lista Inversa Usando função reversed
 numbers = [1, 4, 8 ,0, 10, 20]
 print(list(reversed(numbers)))
 
@@ -20,3 +21,4 @@ print(list(reversed(numbers)))
 [20, 10, 0, 8, 4, 1]
 [20, 10, 0, 8, 4, 1]
 [20, 10, 8, 4, 1, 0]
+[20, 10, 0, 8, 4, 1]
